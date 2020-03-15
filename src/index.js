a75186978423c8625525718bbe25ebe0d3cc03b1
Dylmarcor <dylancorbett@icloud.com>
2020-03-14 17:47:19 -0700

    change root to display App

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'reactDOM';
 import './index.css';
+import App from './App';
+import {BrowserRouter as Router} from 'react-router-dom';
 
-ReactDOM.render(
-    <Header />,
-    <Game />,
-    document.getElementById('root')
-);
+ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
