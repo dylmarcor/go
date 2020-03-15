03e6c0e23ebfd0f32df7832aa935ceb2e1e2158d
Dylmarcor <dylancorbett@icloud.com>
2020-03-15 01:00:07 -0700

    fix grid

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -7,15 +7,39 @@ const Board = (props: MyProps) => {
 
     return (
         <div>
-            <div id="A0"></div>
-            <div id="A1"></div>
-            <div id="A2"></div>
-            <div id="B0"></div>
-            <div id="B1"></div>
-            <div id="B2"></div>
-            <div id="C0"></div>
-            <div id="C1"></div>
-            <div id="C2"></div>
+            <div id="a-row">
+                <div id="A0"></div>
+                <div id="A1"></div>
+                <div id="A2"></div>
+                <div id="A3"></div>
+                <div id="A4"></div>
+                <div id="A5"></div>
+                <div id="A6"></div>
+                <div id="A7"></div>
+                <div id="A8"></div>
+            </div>
+            <div id="b-row">
+                <div id="B0"></div>
+                <div id="B1"></div>
+                <div id="B2"></div>
+                <div id="B3"></div>
+                <div id="B4"></div>
+                <div id="B5"></div>
+                <div id="B6"></div>
+                <div id="B7"></div>
+                <div id="B8"></div>
+            </div>
+            <div id="c-row">
+                <div id="C0"></div>
+                <div id="C1"></div>
+                <div id="C2"></div>
+                <div id="C3"></div>
+                <div id="C4"></div>
+                <div id="C5"></div>
+                <div id="C6"></div>
+                <div id="C7"></div>
+                <div id="C8"></div>
+            </div>
         </div>
     )
 }
